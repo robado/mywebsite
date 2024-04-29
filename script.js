0c96208daadda82ce2b95514d99dfe6a3166be52
robado <ridal.robert@gmail.com>
2024-04-30 00:00:44 +0300

    Added some stuff to work on further. Still planning...

--- script.js
+++ script.js
@@ -6,3 +6,22 @@ function openPage(pageName) {
         })
         .catch(error => console.error('Error:', error));
 }
+
+// Load home.html when loading index.html so the page wouldn't be empty
+window.onload = function() {
+    openPage('home.html');
+}
+
+const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+const storedTheme = localStorage.getItem("theme");
+
+if (storedTheme == "dark" || (prefersDarkScheme.matches && !storedTheme)) {
+    document.body.classList.add("dark-mode");
+}
+
+const toggleDarkModeButton = document.getElementById("toggleDarkMode");
+toggleDarkModeButton.addEventListener("click", function() {
+    document.body.classList.toggle("dark-mode");
+    const theme = document.body.classList.contains("dark-mode") ? "dark" : "light";
+    localStorage.setItem("theme", theme);
+});
